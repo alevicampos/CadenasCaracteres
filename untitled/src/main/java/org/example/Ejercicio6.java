12e07dd3fd8c344840c7a7cb2e8be50af20a0896
DESKTOP\Alejandra <alevicampos@hotmail.com>
2024-10-11 19:24:41 +0200

    Actualización ejercicio 6

--- untitled/src/main/java/org/example/Ejercicio6.java
+++ untitled/src/main/java/org/example/Ejercicio6.java
@@ -14,28 +14,46 @@ public class Ejercicio6 {
         Scanner entrada = new Scanner(System.in);
         String fecha = "DD/MM/AAAA";
         boolean error = true;
+        boolean error2 = true;
+        int diaNumero = 0;
+        int mesNumero = 0;
+        int anyoNumero = 0;
+
+
+
 
 
         while ( error ){
             try{
-                System.out.println("Introduce tu fecha de nacimiento DD/MM/AAAA");
+                System.out.println("Introduce tu fecha de nacimiento DD/MM/AAAA1");
                 fecha = entrada.nextLine();
                 error = false;
             }catch (InputMismatchException Error){
-                System.out.println("La fecha introducida no es válida");
+                System.out.println("La fecha introducida no es válida 1");
                 entrada.nextLine();
             }
         }
 
-            String separador1 = fecha.substring(2,3);
-            String separador2 = fecha.substring(5,6);
 
-                String dia = fecha.substring(0, 2);
-                int diaNumero = Integer.parseInt(dia);
-                String mes = fecha.substring(3, 5);
-                int mesNumero = Integer.parseInt(mes);
-                String anyo = fecha.substring(6, 10);
-                int anyoNumero = Integer.parseInt(anyo);
+            while (error2){
+                try {
+
+                    String dia = fecha.substring(0, 2);
+                    diaNumero = Integer.parseInt(dia);
+                    String mes = fecha.substring(3, 5);
+                    mesNumero = Integer.parseInt(mes);
+                    String anyo = fecha.substring(6, 10);
+                    anyoNumero = Integer.parseInt(anyo);
+                    error2 =false;
+
+                } catch (NumberFormatException Error) {
+                    System.out.println("La fecha introducida no es válida 2");
+                    entrada.nextLine();
+                }
+            }
+                String separador1 = fecha.substring(2,3);
+                String separador2 = fecha.substring(5,6);
+
                 int numeroSuma = diaNumero + mesNumero + anyoNumero;
                 String suma = Integer.toString(numeroSuma);
                 String primerDigito = suma.substring(0, 1);
@@ -50,13 +68,13 @@ public class Ejercicio6 {
 
 
         while ( fecha.length() != 10 || !separador1.equals("/") || !separador2.equals("/") || diaNumero <= 0 || mesNumero <= 0 || anyoNumero <= 0 || diaNumero >31 || mesNumero >12 || anyoNumero > 2025) {
-            System.out.println("La fecha introducida no es válida");
-            System.out.println("Introduce tu fecha de nacimiento DD/MM/AAAA");
+            System.out.println("La fecha introducida no es válida 3");
+            System.out.println("Introduce tu fecha de nacimiento DD/MM/AAAA2");
             fecha = entrada.nextLine();
+            
         }
-            System.out.println("numeroSuerte = " + numeroSuerte);
+            System.out.println("Tu número de la suerte es: " + numeroSuerte);
         }
-
             }
 
 
